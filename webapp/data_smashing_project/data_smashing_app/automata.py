f6e8a4034841f2a67609c918118adc2b521e79d6
Styrof <hectoral_nba@hotmail.com>
2015-05-07 13:45:16 +0200

    automata added

---
+++ webapp/data_smashing_project/data_smashing_app/automata.py
@@ -0,0 +1,48 @@
+from random import random, randrange
+
+class Automata:
+	def __init__(self, proba):
+		self.size = len(proba)
+
+		self.P = [s[:] for s in proba]	
+
+		self.current_state = randrange(self.size)
+
+	def next_state(self):
+		v = random()
+
+		for j in range(self.size):
+			s = sum(P[self.current_state][:j+1])
+
+			if v < s:
+				self.current_state = j
+				return
+
+	def gen_stream(self, size):
+		r = [0] * size
+
+		for i in xrange(size):
+			r[i] = self.current_state
+
+			self.next_state()
+
+		return r
+
+if __name__ == "__main__":
+	P = [[0] * 3 for _ in range(3)]
+
+	P[0][0] = 0.5
+	P[0][1] = 0.3
+	P[0][2] = 0.2
+
+	P[1][0] = 0.25
+	P[1][1] = 0.4
+	P[1][2] = 0.35
+
+	P[2][0] = 0.15
+	P[2][1] = 0.05
+	P[2][2] = 0.8
+
+	a = Automata(P)
+
+	print ''.join(map(str, a.gen_stream(10)))
